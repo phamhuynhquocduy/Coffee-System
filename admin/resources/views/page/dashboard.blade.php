2f5fc164884609a990d7ebdbfdfd8d5bef1e7384
Nguyễn Văn Công <congnv3030@gmail.com>
2021-04-03 19:21:18 +0700

    Update dashboard.blade.php

--- admin/resources/views/page/dashboard.blade.php
+++ admin/resources/views/page/dashboard.blade.php
@@ -27,7 +27,7 @@
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
+                <h3>155</h3>
 
                 <p>Đơn hàng</p>
               </div>
